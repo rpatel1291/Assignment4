7a15850f8d795ce9c30db5ca533908f0a79b967d
Ravi <Rpatel1291@gmail.com>
2019-03-14 19:22:36 -0400

    Update Container.js

--- src/components/Container.js
+++ src/components/Container.js
@@ -11,10 +11,13 @@ class Container extends React.Component {
         super(props)
         this.state = {
             text:"",
-            filename:"",
+            filename:"Default",
+            fileextention:"html",
+            // fullFileName:filename+"."+fileextention,
             html:"",
         }
         this.handleFileName = this.handleFileName.bind(this)
+        this.handleFileExt = this.handleFileExt.bind(this)
     }
     updateValue(text){
         this.setState({text})
@@ -22,14 +25,14 @@ class Container extends React.Component {
     handleFileName(event){
         this.setState({filename: event.target.value})
     }
+    handleFileExt(event){
+        this.setState({fileextention: event.target.value})
+    }
 
     render(){
         console.log(this.state.text)
         return(
             <div className="Container">
-                <nav className="navbar">
-                    {/* Enter download button */}
-                </nav>
                 <SplitPane split="vertical" defaultSize="50%">
                     <div className="input-section-pane">
                         <h3 id="editor-section-id">Editor: </h3>
@@ -38,17 +41,29 @@ class Container extends React.Component {
                         />
                     </div>
                     <div className="view-pane">
-                        <h3 
-                            id="preview-section-id"
-                        >
-                            Preview File: {this.state.filename}
+                        <h3 id="preview-section-id">
+                            Preview File: {this.state.filename}.{this.state.fileextention}
                         </h3>
                         <label>Enter File Name:
-                            <input 
+                            <input
+                                id="filename-input" 
                                 type="text"
                                 placeholder={this.state.filename}
                                 onChange={this.handleFileName.bind(this)}
                             />
+                            <select
+                                id="file-extention-selector"
+                                onChange={this.handleFileExt.bind(this)}
+                            >
+                                <option value="html">HTML (.html)</option>
+                                <option value="md">MarkDown (.md)</option>
+                            </select>
+                            <button
+                                id="download-button"
+                                // Add a onclick function to download
+                            >
+                                Download
+                            </button>
                         </label>
                         <OutputSection
                             sendText={this.state.text}  
